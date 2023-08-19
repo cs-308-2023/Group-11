da414a4ab58262535c55fffefaaa3504f3f3f47a
cs-308-2023 <adityaruhela2003@gmail.com>
2023-08-19 18:39:04 +0530

    part 5

--- factorial.cpp
+++ factorial.cpp
@@ -3,7 +3,13 @@
 using namespace std;
 int factorial(int n){
     if(n==0) return 1;
-    
+    if(n<0) {
+        while(n<0){
+        cout<<"Please enter a positive number : ";
+        cin>>n;
+        cout<<endl;
+        }
+    }
     
     if(n!=1){
 	return(n * factorial(n-1));
