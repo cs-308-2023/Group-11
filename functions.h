c56b41b9f5423e8b8250d35aee347d78b1d600c3
sarvesh543 <sarveshjadhav543@gmail.com>
2023-08-28 10:29:42 +0530

    step_4 to step_6

--- functions.h
+++ functions.h
@@ -1,3 +1,4 @@
 void print_hello();
 int factorial(int n);
 int gcd_nonrec(int a, int b);
+int gcd_rec(int a, int b);
--- main.cpp
+++ main.cpp
@@ -10,5 +10,9 @@ int main() {
     cin >> n;
     cout<<"The factorial of %d is %d\n"<<n<<factorial(n)<<endl;
 
+    int x = 56, y = 98;
+    cout<<"The gcd by non recursive function of %d and %d is %d"<<x<<y<<gcd_nonrec(x, y)<<endl;
+    cout<<"The gcd by recursive function of %d and %d is %d"<<x<<y<<gcd_rec(x, y)<<endl;
+
     return 0;
 }
